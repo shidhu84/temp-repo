e258d97d9655c7a78b616aefa3ad678b6f557783
shidhu84 <shidyenure@yahoo.com>
2024-06-25 20:44:08 +0530

    Added Calculation to convert temp fahrenheit into Celcius

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState();
+  const [valueInCelsius, setvalueInCelsius] = useState(0);
+  const handleChange = (e) => {
+    setFormData(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+    let calculate = ((formData - 32) * 5) / 9;
+    setvalueInCelsius(calculate.toFixed(2));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="number" id="temp" onChange={handleChange} />
+        <button type="submit"> Submit</button>
+      </form>
+      <h2>result in Celsius: {valueInCelsius} </h2>
     </div>
   );
 }
